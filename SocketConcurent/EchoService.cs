d6d669331c54c9767a0508c768226cbb58a96690
AbxdHxcks <abedbadr1999@hotmail.com>
2019-09-12 13:31:22 +0200

    Add project files.

---
+++ SocketConcurent/EchoService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketConcurent
+{
+    public class EchoService
+    {
+        private TcpClient _connectionSocket;
+
+        public EchoService(TcpClient connection)
+        {
+            _connectionSocket = connection;
+        }
+
+        public void DoIt()
+        {
+            Stream ns = _connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+        }
+    }
+}
---
+++ SocketConcurent/TCPEchoServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketConcurent
+{
+    class TCPEchoServer
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+            Console.WriteLine("Server started");
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server activated");
+
+                EchoService echoService = new EchoService(connectionSocket);
+
+                echoService.DoIt();
+            }
+        }
+    }
+}
